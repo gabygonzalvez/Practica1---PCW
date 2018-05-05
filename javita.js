d8bd4ff93d1ee27b98766ea256e86f8f3e2c01df
Gabriela <gabygonzalvez@gmail.com>
2018-05-05 17:47:48 +0200

    busqueda rapida e index

--- javita.js
+++ javita.js
@@ -1,7 +1,7 @@
 /*
 function peticionFetchAPI_POST(form_HTML, clave){
-	var url = 'rest/login/', 
-	valor=new FormData(form_HTML), 
+	var url = 'rest/login/',
+	valor=new FormData(form_HTML),
 	init = {method:'post', body:valor, headers:{'Authorization':clave}};
 
 	fetch(url, init).then(function(respuesta){
@@ -52,19 +52,19 @@ function hacerlogin(){
 	let url = './rest/login/';
 	let loginForm = document.querySelectorAll("form")[0];
 	let value= new FormData(loginForm);
-	
-	
+
+
 	//fd.append('login', 'usuario2');
 	//fd.append('pwd', 'usuario2');
 
 	fetch(url, {'method':'POST', 'body':value}).then(function(respuesta){
-		
+
 		if(!respuesta.ok){
 			//respuesta.json().then(function(datos){
 			console.log('No esta bien hecho, no se han encontrado los datos en la database');
 			document.getElementById('usuariologin').focus();
-			
-			
+
+
 			document.getElementById('alerta').innerHTML = '<p>Los datos son incorrectos. </p>' //muestra nueva_receta
 			//let html='<p> El usuario o contra seña es inkorecto2</p>';
 			location.href='#openModal';
@@ -75,13 +75,13 @@ function hacerlogin(){
 				console.log(datos);
 				sessionStorage.setItem('usuario', JSON.stringify(datos));
 				location.href='index.html';
-				barradenav();	
+				barradenav();
 			});
 		}
 	}, function(respuesta){
 		console.log('NO HA HECHO EL FECH');
 	});
-	
+
 	return false;
 }
 
@@ -100,6 +100,220 @@ function vaciar(){
 	sessionStorage.clear();
 }
 
+/*------- FUNCIONES RELACIONADAS CON INDEX -------*/
+
+function cargarUltimas(){
+
+	var url = 'rest/receta/?u=6';
+	mostrarRecetas(url,'contenidoIndex');
+
+}
+
+function cargarUltimasBusq() {
+
+	var url = 'rest/receta/?u=6';
+	mostrarRecetas(url,'contenidoResultado');
+
+}
+
+function mandarBusqueda() {
+
+	var param = document.getElementById("buscar").value;
+	window.location.replace('./buscar.html?buscar='+ param);
+
+}
+
+function busquedita() {
+
+	if(window.location.href!=='http://localhost/pcw/practica02/buscar.html'){
+		var parametros = window.location.href.split("=")[1];
+		console.log(parametros);
+		if(parametros===''){
+			cargarUltimasBusq();
+		}
+		else {
+			// hacer peticion fetch get con la siguiente url
+			var url = 'rest/receta/?t=' + parametros;
+			mostrarRecetas(url,'contenidoResultado');
+		}
+	}
+	else {
+		cargarUltimasBusq();
+	}
+
+}
+
+
+
+/*------- FUNCIONES RELACIONADAS CON REGISTRO -------*/
+
+function registramen(formulario){
+	console.log("hace cosas llega aqui");
+	let url = './rest/usuario/';
+	let value= new FormData(formulario);
+
+	var x=1
+	var comprobacion=false;
+
+	for(var valor of value.values()){
+		if(x==2){
+			pwd=valor;
+		}
+		if(x==3){
+			pwd2=valor;
+		}
+		x++;
+	}
+	if(pwd==pwd2){
+		comprobacion=true;
+	fetch(url, {'method':'POST', 'body':value}).then(function(respuesta){
+
+			if(!respuesta.ok){
+				//respuesta.json().then(function(datos){
+				respuesta.json().then(function(datos){
+					console.log(datos);
+
+				})
+			}
+			else{
+				respuesta.json().then(function(datos){
+					console.log(datos);
+					location.href='login.html';
+				});
+			}
+		}, function(respuesta){
+			console.log('NO HA HECHO EL FECH');
+		});
+
+		return false;
+
+	}
+	else{
+			console.log("La contraseña esta mal, escribela de nuevo");
+			location.href='#openModal';
+	}
+}
+
+/*------- FUNCIONES RELACIONADAS CON BUSQUEDA RAPIDA -------*/
+
+
+/*function busquedita(){
+	console.log("entra a busquedita obvizmente");
+  	var url = 'rest/receta/?t=';
+
+	console.log(url);
+	var urlfin=document.getElementById("buscar").value;
+	urlllevar='buscar.html/u?=' + urlfin;
+	console.log(urlllevar);
+	location.href=urlllevar;
+
+  fetch(url).then(function(response){
+    if(!response.ok){ // if(response.status!=200)
+    		console.log("SALE UN ERROR estoy enfadada");
+
+      console.log('Error(' + response.status + '): ' + response.statusText);
+      return;
+    }
+    	console.log("AQUI NO SALE ERROR ira no seeeeeeeeeeee");
+
+    response.json().then(function(data) {
+    console.log(data);
+
+  });
+  }).catch(function(errorcito) {
+    console.log('Fetch Error: ', errorcito);
+  });
+}*/
+
+
+
+
+
+
+
+
+
+/*
+	var url = './buscar.html?buscar=' + parametros;
+	var parametros = getParameterByNam('buscar');
+	/*var param='?buscar=' + parametros;
+	console.log('hola3');
+
+	console.log(url);
+
+
+	mostrarRecetas(url);
+
+}*/
+
+/*function getParameterByName(name) {
+
+	console.log('hola2');
+	name = name.replace(/[\[]/,"\\\[").replace(/[\]]/,"\\\]");
+	let href = window.location;
+	var regexS = "[\\?&]"+name+"=([^&#]*)";
+	var regex = new RegExp(regexS);
+	var results = regex.exec(href);
+
+	if( results == null )
+		return "";
+	else
+		return decodeURIComponent(results[1].replace(/\+/g, " "));
+	var regex=new RegExp("[\\?&]" + name + "=([^&#]*)"),
+	results = regex.exec(location.search);
+	return results===null ? "": decodeURIComponent(results[1].replace(/\+/g, " "));
+}*/
+
+function mostrarRecetas(url, sitio) {
+
+	fetch(url).then(function(respuesta){
+                if(!respuesta.ok){
+                    console.log('Error(' + respuesta.status + '): ' + respuesta.statusText);
+                    return;
+                }
+								else {
+												respuesta.json().then(function(datos){
+
+			                    var elemento = document.getElementById(sitio);
+			                    if (elemento) {
+			                        for( let i = 0 ; i < datos.FILAS.length; i++){
+			                            var receta = datos.FILAS[i];
+			                            elemento.innerHTML = elemento.innerHTML +
+
+						                    	'<section>' +
+																	'<h4><a href="receta.html?id=' + receta.id + '" class="tituloHome"> ' + receta.nombre + '</a></h4>' +
+																	'<a href="receta.html?id=' + receta.id + '"><img src="fotos/' + receta.fichero + '" class="fotoHome" alt="' + receta.descripcion_foto + '"></a>' +
+																	'<ul>' +
+																	'<li><b><a href="buscar.html?id=' + receta.autor + '">Autor: </a></b> '+ receta.autor +' </li>' +
+																	'<li><b>Likes</b> '+ receta.positivos +' </li>'+
+																	'<li><b>Dislikes</b> '+ receta.negativos +' </li>'+
+																	'<li><b>Fecha</b> '+ receta.fecha +' </li>'+
+																	'</ul>'+
+																	'</section>'
+		                        }
+		                    	}
+
+
+	                			});
+
+										}
+
+
+            });
+}
+
+
+
+/*------- FUNCIONES RELACIONADAS CON RECETA -------*/
+
+
+function cargarReceta() {
+	if(window.location.search){
+
+		console.log(location.search);
+		let url = 'rest/receta/'
+	}
+}
 
 /*------- FUNCIONES RELACIONADAS CON NUEVA RECETA -------*/
 
@@ -109,9 +323,9 @@ function compruebo(){
 		console.log("perfe");
 	}
 	else{
-	
+
 		location.href="index.html";
-		
+
 		console.log("perfeno");
 
 	}
@@ -124,8 +338,8 @@ function coments(){
 		document.getElementById("comenta").innerHTML='<p> Para poder escribir un comentario debes estar <a href="login.html">logueado</a></p>'
 	}
 	else{
-	
-		
+
+
 	}
 }
 
@@ -134,7 +348,7 @@ function coments(){
     var foto = document.getElementById(idraro[0].id);
     var tamanyo=300*1024;
     foto.src = URL.createObjectURL(event.target.files[0]);
- 
+
   }
 
  function eliminar(id){
@@ -182,7 +396,7 @@ function nuevarecetasubida(){
     valor = new FormData();
     var opcion = document.querySelector('select[id=dificult]').value;
      console.log(opcion);
-    
+
     if(opcion =='Baja') {
 	  numero = 0;
     }else if(opcion =='Media') {
@@ -236,211 +450,3 @@ function comprobadorsesion(){
 		console.log("NO PUEDES ENTRAR")
 	}
 }*/
-
-/*------- FUNCIONES RELACIONADAS CON REGISTRO -------*/
-
-function registramen(formulario){
-	console.log("hace cosas llega aqui");
-	let url = './rest/usuario/';
-	let value= new FormData(formulario);
-
-	var x=1
-	var comprobacion=false;
-
-	for(var valor of value.values()){
-		if(x==2){
-			pwd=valor;
-		}
-		if(x==3){
-			pwd2=valor;
-		}
-		x++;
-	}
-	if(pwd==pwd2){
-		comprobacion=true;
-	fetch(url, {'method':'POST', 'body':value}).then(function(respuesta){
-			
-			if(!respuesta.ok){
-				//respuesta.json().then(function(datos){
-				respuesta.json().then(function(datos){
-					console.log(datos);
-					
-				})
-			}
-			else{
-				respuesta.json().then(function(datos){
-					console.log(datos);
-					location.href='login.html';
-				});
-			}
-		}, function(respuesta){
-			console.log('NO HA HECHO EL FECH');
-		});
-		
-		return false;
-
-	}
-	else{
-			console.log("La contraseña esta mal, escribela de nuevo");
-			location.href='#openModal';
-	}
-}
-
-/*------- FUNCIONES RELACIONADAS CON BUSQUEDA RAPIDA -------*/
-
-/*function redirecBuscar(formulario) {
-
-	busquedaRapida(formulario);
-	location.href='buscar.html';
-
-}*/
-
-
-
-
-/*function busquedita(){
-	console.log("entra a busquedita obvizmente");
-  	var url = 'rest/receta/?t='; 
-	
-	console.log(url);
-	var urlfin=document.getElementById("buscar").value;
-	urlllevar='buscar.html/u?=' + urlfin;
-	console.log(urlllevar);
-	location.href=urlllevar;
-
-  fetch(url).then(function(response){
-    if(!response.ok){ // if(response.status!=200)
-    		console.log("SALE UN ERROR estoy enfadada");
-
-      console.log('Error(' + response.status + '): ' + response.statusText);
-      return;
-    }
-    	console.log("AQUI NO SALE ERROR ira no seeeeeeeeeeee");
-
-    response.json().then(function(data) { 
-    console.log(data); 
-
-  });
-  }).catch(function(errorcito) {
-    console.log('Fetch Error: ', errorcito);
-  });
-}*/
-
-function mandarBusqueda() {
-
-	var param = document.getElementById("buscar").value;
-
-	window.location.replace('./buscar.html?buscar='+ param);
-	
-}
-
-function busquedita() {
-
-
-	var parametros = window.location.href.split("=")[1];
-
-	console.log(parametros);
-
-	if(parametros===' '){
-		// cargar las ultimas 6 igual que cargar index
-	}
-	else {
-		// hacer peticion fetch get con la siguiente url
-		var url = 'rest/receta/?t=' + parametros;
-	}
-
-
-
-}
-
-
-
-
-
-
-
-/*
-	var url = './buscar.html?buscar=' + parametros;
-	var parametros = getParameterByNam('buscar');
-	/*var param='?buscar=' + parametros;
-	console.log('hola3');
-
-	console.log(url);
-
-
-	mostrarRecetas(url);
-	
-}*/
-
-/*function getParameterByName(name) {
-
-	console.log('hola2');
-	name = name.replace(/[\[]/,"\\\[").replace(/[\]]/,"\\\]");
-	let href = window.location;
-	var regexS = "[\\?&]"+name+"=([^&#]*)";
-	var regex = new RegExp(regexS);
-	var results = regex.exec(href);
-
-	if( results == null )
-		return "";
-	else
-		return decodeURIComponent(results[1].replace(/\+/g, " "));
-	var regex=new RegExp("[\\?&]" + name + "=([^&#]*)"),
-	results = regex.exec(location.search);
-	return results===null ? "": decodeURIComponent(results[1].replace(/\+/g, " "));
-}*/
-
-function mostrarRecetas(url) {
-	console.log('hola');
-
-	fetch(url).then(function(respuesta){
-                if(!respuesta.ok){
-                    console.log('Error(' + respuesta.status + '): ' + respuesta.statusText);
-                    return;
-                }
-                
-                	console.log("ENTRA Y LO HACE BIEN");
-                	location.href="buscar.html";
-                		console.log('holaaaaaaa');
-                	respuesta.json().then(function(datos){
-                    var elemento;
-                    localStorage.setItem("n_resultados", datos.TOTAL_COINCIDENCIAS);
-                    console.log("storage mostrar: " + localStorage.getItem("n_resultados"));
-                    elemento = document.getElementById("contenidoIndex");
-                    if (elemento) {
-                        for( let i = 0 ; i < datos.FILAS.length; i++){
-                            var receta = datos.FILAS[i];
-                            elemento.innerHTML = elemento.innerHTML +
-                     
-                    	'<section>' +
-						'<h4><a href="receta.html?id=' + receta.id + '" class="tituloHome"> ' + receta.nombre + '</a></h4>' +
-						'<a href="receta.html?id=' + receta.id + '"><img src="fotos/ ' + receta.fichero + '" class="fotoHome" alt="' + receta.descripcion_foto + '"></a>' +
-						'<ul>'
-						'<li><b><a href="buscar.html?id=' + receta.autor + '">Autor: </a></b> '+ receta.autor +' </li>' +
-						'<li><b>Likes</b> '+ receta.positivos +' </li>'+
-						'<li><b>Dislikes</b> '+ receta.negativos +' </li>'+
-						'<li><b>Fecha</b> '+ receta.fecha +' </li>'+
-						'</ul>'+
-						'</section>;'
-                        }
-                    }
-
-                    
-                });
-
-                
-            }); console.log("mal");
-}
-
-
-	
-/*------- FUNCIONES RELACIONADAS CON RECETA -------*/
-
-
-function cargarReceta() {
-	if(window.location.search){
-
-		console.log(location.search);
-		let url = 'rest/receta/'
-	}
-}
